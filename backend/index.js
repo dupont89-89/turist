72798c802cf4eb3a0d15e64dac322574e9e300aa
dupont89 <dupont89-89@yandex.ru>
2023-06-20 00:13:58 +0500

    server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
- 
 const PORT = process.env.PORT || 3010;
 const app = express();
- 
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
- 
+
+const cityName = require('./api/city.json');
+
+app.get('/api/city', (req, res) => {
+  res.json({ data: cityName });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
