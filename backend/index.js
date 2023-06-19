b58e91656000e5722e705a0b5084cc25ad1eaf41
dupont89 <dupont89-89@yandex.ru>
2023-06-19 22:55:48 +0500

    Turist

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+ 
+const PORT = process.env.PORT || 3010;
+const app = express();
+ 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import s from "../Footer/Footer.module.css"
+
+export function Footer(props) {
+
+    return (
+        <section className={s.block__menu}>
+          <div className="col__menu__footer">
+            <ul className="col__menu__footer__ul">
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+            </ul>
+          </div>
+          <div className="block__menu__footer">
+            <ul className="col__menu__ul">
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+            </ul>
+          </div>
+          <div className="block__menu__footer">
+            <ul className="col__menu__ul">
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+            </ul>
+          </div>
+          <div className="block__menu__footer">
+            <ul className="col__menu__ul">
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+                <li className="menu__item">Меню пункт</li>
+            </ul>
+          </div>    
+        </section>
+      );
+}
